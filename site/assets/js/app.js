ec370f3bbbf62abe5804ac655f1680edf630369f
Anton Gorban <anton.stroncii@gmail.com>
2017-03-12 01:19:53 +0200

    routing

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -8,12 +8,56 @@ var app = angular.module('app', ['ngRoute'])
       templateUrl: 'templates/history.html'
   })
   .when('/news', {
-    templateUrl: 'news.html'
+    templateUrl: 'templates/news.html'
   })
   .when('/admin', {
     templateUrl: 'templates/admin.html'
   })
-
+  .when('/dean', {
+    templateUrl: 'templates/dean.html'
+  })
+  .when('/pmi', {
+    templateUrl: 'templates/pmi.html'
+  })
+  .when('/kn', {
+    templateUrl: 'templates/kn.html'
+  })
+  .when('/philosophy', {
+    templateUrl: 'templates/philosophy.html'
+  })
+  .when('/schedule', {
+    templateUrl: 'templates/schedule.html'
+  })
+  .when('/consultations', {
+    templateUrl: 'templates/consultations.html'
+  })
+  .when('/process', {
+    templateUrl: 'templates/process.html'
+  })
+  .when('/plans', {
+    templateUrl: 'templates/plans.html'
+  })
+  .when('/organizations', {
+    templateUrl: 'templates/organizations.html'
+  })
+  .when('/documents', {
+    templateUrl: 'templates/documents.html'
+  })
+  .when('/specialties', {
+    templateUrl: 'templates/specialties.html'
+  })
+  .when('/cooperation', {
+    templateUrl: 'templates/cooperation.html'
+  })
+  .when('/aspirantur', {
+    templateUrl: 'templates/aspirantur.html'
+  })
+  .when('/contacts', {
+    templateUrl: 'templates/contacts.html'
+  })
+  .when('/news-item', {
+    templateUrl: 'templates/news-item.html'
+  })
 }]);
 
 

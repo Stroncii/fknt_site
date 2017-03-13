9c5f2f62af620ed3d10a1128384a85fcba9a5f69
Anton Gorban <anton.stroncii@gmail.com>
2017-03-13 21:55:23 +0200

    slice news

--- site/assets/js/controllers/newsController.js
+++ site/assets/js/controllers/newsController.js
@@ -4,7 +4,9 @@ angular.module('app')
 
   appFactory.getNews().then(function(data) {
     $scope.firstNews = data.news[0];
-    $scope.news = data.news.slice(-data.news.length-1);
+    $scope.news = data.news;
+    $scope.news = $scope.news.slice(-($scope.news.length-1))
+    console.log($scope.news);
   });
   appFactory.getLastNews().then(function(data) {
     console.log(data);

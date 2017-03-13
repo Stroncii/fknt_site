106fc620508688c8a6b06a6900a5672117029b9f
Anton Gorban <anton.stroncii@gmail.com>
2017-03-13 22:40:40 +0200

    changes

--- site/assets/js/controllers/newsController.js
+++ site/assets/js/controllers/newsController.js
@@ -6,10 +6,8 @@ angular.module('app')
     $scope.firstNews = data.news[0];
     $scope.news = data.news;
     $scope.news = $scope.news.slice(-($scope.news.length-1));
-    console.log($scope.news);
   });
   appFactory.getLastNews().then(function(data) {
-    console.log(data);
     $scope.lastNews = data.news;
     $scope.smallLastNews = $scope.lastNews.slice(-3);
   });

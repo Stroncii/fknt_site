7aabe46f80b019cf5096420de8c929e260f3e883
Anton Gorban <anton.stroncii@gmail.com>
2017-04-23 14:24:04 +0300

    schedules

--- site/assets/js/controllers/scheduleController.js
+++ site/assets/js/controllers/scheduleController.js
@@ -2,27 +2,72 @@ angular.module('app')
 .controller('scheduleController',['$scope', '$http', 'pdfFactory', '$sce' , function($scope, $http, pdfFactory, $sce){
   
   function getDefaults () {
+    $scope.mode = 'add';
     $scope.editedGroup = {
-      name_uk: '',
-      name_en: '',
-      name_ru: ''
+      uk: '',
+      en: '',
+      ru: '',      
     }
     getSchedules();
   }
 
   function getSchedules () {
     pdfFactory.getSchedule().then((data) => {
-      console.log('schedule.data');
-      console.log(data);
       $scope.schedule = data;
+      $scope.editedGroup.id = data[0].department_id;
     });
   };
-
+  
+  getDefaults();
 
   $scope.editPill = function (pill) {
     console.log(pill);
+    $scope.editedGroup = angular.copy(pill.group);
+    $scope.mode = 'edit';
   };
 
+
+  $scope.setActive = function (id) {
+    $scope.editedGroup.id = id;
+    console.log($scope.editedGroup);
+  };
+
+  $scope.cancel = function () {
+    $scope.mode = 'add';
+    getDefaults();
+  };
+
+  $scope.add = function () {
+
+  };
+
+
+
+
+
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'upload.php'
+        });
+
+        // FILTERS
+      
+        // a sync filter
+        uploader.filters.push({
+            name: 'syncFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                console.log('syncFilter');
+                return this.queue.length < 10;
+            }
+        });
+      
+        // an async filter
+        uploader.filters.push({
+            name: 'asyncFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options, deferred) {
+                console.log('asyncFilter');
+                setTimeout(deferred.resolve, 1e3);
+            }
+        });
 
 
 

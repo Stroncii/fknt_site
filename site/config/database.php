a4c420167a2c23d24a2c8f94f5c9209dd185c92c
Daniella <daniella.brovkina@gmail.com>
2017-05-12 00:36:19 +0200

    update

--- site/config/database.php
+++ site/config/database.php
@@ -4,18 +4,18 @@
 class Database{ 
  
     // specify your own database credentials 
-    /*private $host = "sql200.byethost18.com"; 
+    private $host = "sql200.byethost18.com"; 
     private $db_name = "b18_19812321_knt"; 
     private $username = "b18_19812321"; 
-    private $password = "ab07You09";*/
+    private $password = "ab07You09";
     /*private $host = "localhost"; 
     private $db_name = "knt"; 
     private $username = "knt"; 
     private $password = "dYWpTrwq";*/
-    private $host = "localhost";
+    /*private $host = "localhost";
    	private $db_name = "knt"; 
     private $username = "root"; 
-    private $password = "root"; 
+    private $password = "root"; */
     public $conn; 
  
 
